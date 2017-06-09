def9a6ca0a0ed550b8a01d33a5f9f1e08d5ced8d
u4bi <u4bi@u4bi.com>
2017-06-10 04:17:21 +0900

    모바일 터치 인터렉션 기본 이벤트 리스너 등록

--- src/core/Mobile.js
+++ src/core/Mobile.js
@@ -1,7 +1,47 @@
 export class Mobile {
     
-    constructor(){
+    constructor(element){
+
+        this.element = element || window;
+        this.isTouch = false;
  
+        this.touch = {
+            delta : {},
+            move  : {},
+            pinch : {}
+        };
+
+        this.touchDown = this.touchDown.bind(this);
+        this.touchUp   = this.touchUp.bind(this);
+        this.touchMove = this.touchMove.bind(this);
+
+        this.init();        
+        
+    }
+
+
+    init(){
+        console.log('init');
+        this.element.addEventListener('touchstart', this.touchDown);
+
+        window.addEventListener('touchend', this.touchUp);
+        window.addEventListener('touchmove', this.touchMove);
+
+        this.isTouch = false;        
+
+    }
+
+    touchDown(e){
+        console.log(this);
+        
+    }
+
+    touchUp(e){
+
+    }
+
+    touchMove(e){
+
     }
 
 }
