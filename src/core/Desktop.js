c6a9d64280e30a2136d1970c07ded58c1d072e68
u4bi <u4bi@u4bi.com>
2017-06-09 23:42:03 +0900

    데스크탑 터치 인터렉션 기본 이벤트 리스너 등록

--- src/core/Desktop.js
+++ src/core/Desktop.js
@@ -1,7 +1,39 @@
 export class Desktop {
-    
-    constructor(){
- 
+     
+    constructor(element){
+        
+        this.element = element || window;
+        this.isTouch = false;
+
+        this.touch = {
+            delta : {},
+            move : {}
+        };
+        
+        this.init();
+    }
+
+
+    init(){
+        console.log('init');
+        this.element.addEventListener('mousedown', this.mouseDown);
+        
+        window.addEventListener('mouseup', this.mouseUp);
+        window.addEventListener('mousemove', this.mouseMove);
+        
+        this.isTouch = false;
+    }
+
+    mouseDown(e){
+
+    }
+
+    mouseUp(e){
+
+    }
+
+    mouseMove(e){
+
     }
 
 } 
