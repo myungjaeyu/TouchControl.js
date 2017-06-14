7e1b101a494286a0f42d568b38e23f7d3a41d395
Yu Myungjae <u4bi@letsee.io>
2017-06-14 22:00:37 +0900

    mobile drag

--- src/modules/Mobile.js
+++ src/modules/Mobile.js
@@ -6,6 +6,7 @@ export class Mobile {
         this.isTouch = false;
         this.isMove = false;        
         this.isPinch = false;
+        this.isDrag  = false;
  
         this.touch = {
             delta : { 
@@ -21,9 +22,19 @@ export class Mobile {
                 end   : 0.0,
                 zoom  : 0.0
             },
+            drag :{
+                pos   : {
+                    start : 0.0,
+                    end : 0.0
+                },
+                start : 0.0,
+                end   : 0.0,
+                delta  : 0.0
+            },
             pitch : 0.0,
             yaw   : 0.0,
             type  : '',
+            len   : 0,
             direction : {
                 north : false,
                 south : false,
@@ -70,6 +81,8 @@ export class Mobile {
  
             this.touch.pinch.start = Math.sqrt(this.touch.pinch.x * this.touch.pinch.x + this.touch.pinch.y * this.touch.pinch.y);
             // console.log('pinch start' ,this.touch.pinch.start);
+            this.touch.drag.pos.start = e.touches[0].pageY;
+            this.touch.drag.start = this.touch.pinch.x;
             return;
         }
         
@@ -84,6 +97,7 @@ export class Mobile {
         this.isTouch = false;
         this.isMove  = false;
         this.isPinch = false;
+        this.isDrag  = false;
         this.touch.direction = {
             north : false,
             south : false,
@@ -99,25 +113,53 @@ export class Mobile {
     }
 
     touchMove(e){
+        console.log('len ' ,e.touches.length);
+
         if(!this.isTouch) return;   
         // console.log('touch move');
 
-        if(e.touches.length > 1){
+        this.touch.len = e.touches.length;
+
+        if(e.touches.length === 2){
             
-            this.touch.pinch.x = e.touches[0].pageX - e.touches[1].pageX;
-            this.touch.pinch.y = e.touches[0].pageY - e.touches[1].pageY;
-            // console.log('pinch ' ,this.touch.pinch);
+            this.touch.drag.end = e.touches[0].pageX - e.touches[1].pageX;
+            
+            let drag = (this.touch.drag.start - this.touch.drag.end).toFixed(1);
+            this.touch.drag.start = this.touch.drag.end;
+            // console.log(drag);
+            
+            let 
+                limit = 1;
 
-            this.touch.pinch.end = Math.sqrt(this.touch.pinch.x * this.touch.pinch.x + this.touch.pinch.y * this.touch.pinch.y);
-            // console.log('pinch end ' ,this.touch.pinch.end);
+            if(drag > -limit && drag < limit){
+                // console.log('drag');
 
+                this.touch.type = 'drag';
+                this.isDrag = true;
 
-            this.touch.pinch.zoom = this.touch.pinch.start / this.touch.pinch.end;
-            // console.log('pinch zoom', this.touch.pinch.zoom);
+                this.touch.drag.pos.end = e.touches[0].pageY;
+                this.touch.drag.delta = Math.sqrt(this.touch.drag.pos.start / this.touch.drag.pos.end);
 
-            this.touch.type = 'pinch';
-            this.isPinch = true;
-            return;
+                // console.log('drag delta', this.touch.drag.delta);
+                this.touch.drag.pos.start = this.touch.drag.pos.end;
+
+            }else{
+                // console.log('pinch');
+
+                this.touch.pinch.x = e.touches[0].pageX - e.touches[1].pageX;
+                this.touch.pinch.y = e.touches[0].pageY - e.touches[1].pageY;
+                // console.log('pinch ' ,this.touch.pinch);
+
+                this.touch.pinch.end = Math.sqrt(this.touch.pinch.x * this.touch.pinch.x + this.touch.pinch.y * this.touch.pinch.y);
+
+                this.touch.pinch.zoom = this.touch.pinch.start / this.touch.pinch.end;
+                this.touch.pinch.start = this.touch.pinch.end;
+                // console.log('pinch zoom', this.touch.pinch.zoom);
+
+                this.touch.type = 'pinch';
+                this.isPinch = true;
+            }
+            
         }
 
         this.isMove  = true;
